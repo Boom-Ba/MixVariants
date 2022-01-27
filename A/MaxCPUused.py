9e88e69824012b113c54757a70167dccbba55f8e
Boom-Ba <70102254+Boom-Ba@users.noreply.github.com>
2022-01-27 09:10:18 -0500

    Update MaxCPUused.py

--- A/MaxCPUused.py
+++ A/MaxCPUused.py
@@ -1,11 +1,8 @@
 #Maximum CPU used
 """
-Rule1: if no two job overlaps, maxCPU used is max of each of them
-Rule2: if overlaps, the maximum is sum of overlapped ranges
+Rule 1: the maximum CPU for two overlap jobs is the sum of each CPU used. 
+Rule 2: The maximum CPU for non-overlap jobs is the CPU that is currently in use.
 """
-
-#(start, end, CPU used)
-#1 2 4 5, maxcpu used is 7
 #1.minheap
 import heapq
 def MaxCPUuse(jobs):
@@ -14,11 +11,12 @@ def MaxCPUuse(jobs):
   currCPUUse=0
   maxCPUUse=0
   for j in jobs:
-    #(1,4,3),(2,5,4),(7,9,6)]
     #check if the top of job standing at heap can be finished before current job starts
     while minheap and len(minheap)>0 and minheap[0][1]<=j[0]:
+      #if current job starts later than prev, the Current CPU used - previous use
       currCPUUse-=minheap[0][2]
       heapq.heappop(minheap)
+      
     heapq.heappush(minheap,(j[0],j[1],j[2]))
     currCPUUse+=j[2]
     maxCPUUse=max(maxCPUUse,currCPUUse)
